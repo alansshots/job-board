cb274236df12a76d1c4f94db7cb34e61c4df1d20
alansshots <alengospodinov@gmail.com>
2023-04-21 14:51:29 +0200

    offers page

--- job-board/src/components/Navbar.jsx
+++ job-board/src/components/Navbar.jsx
@@ -10,17 +10,17 @@ const Navbar = () => {
            <img src='' alt="logo" className='w-24 sm:w-36'/>
         </Link>
 
-     <ul className="flex items-center gap-2 text-medium font-semibold text-gray-800">
-        <NavLink to="/" className={({ isActive }) => isActive ? 'm-4 border-b-2 border-gray-300' : 'm-4'}>Обяви</NavLink>
+     <ul className="flex items-center gap-2 text-medium font-bold text-gray-800 uppercase">
+        <NavLink to="/" className={({ isActive }) => isActive ? 'm-4 border-b-2 border-yellow-500' : 'm-4'}>Обяви</NavLink>
 
-        <NavLink  to="/blog" className={({ isActive }) => isActive ? 'm-4 border-b-2 border-gray-300' : 'm-4'}>Блог</NavLink>
+        <NavLink  to="/blog" className={({ isActive }) => isActive ? 'm-4 border-b-2 border-yellow-500' : 'm-4'}>Блог</NavLink>
         <NavLink to="/register">
-        <button className="flex items-center px-6 py-2  transition ease-in duration-200 uppercase rounded-full bg-gray-800 hover:bg-white text-white hover:text-gray-800 border-2 border-gray-900 focus:outline-none">          
+        <button className="flex items-center px-4 py-1  transition ease-in duration-200 uppercase text-sm rounded-full bg-gray-800 hover:bg-white text-white hover:text-gray-800 border-2 border-gray-900 focus:outline-none">          
             Регистрация
         </button>
         </NavLink>
 
-        <button className="flex items-center px-6 py-2  transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none">          
+        <button className="flex items-center px-4 py-1  transition ease-in duration-200 uppercase text-sm rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none">          
             Вход
         </button>
 
