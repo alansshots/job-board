28ac7c598df2487fd5143b567c40683c66303ab9
alansshots <alengospodinov@gmail.com>
2023-04-30 23:04:32 +0200

    filters update

--- job-board/src/components/Filters.jsx
+++ job-board/src/components/Filters.jsx
@@ -1,128 +1,161 @@
 import React from 'react'
-import { ChevronDown } from 'react-feather';
+import { ChevronDown, MapPin, Briefcase, DollarSign, XCircle, CheckCircle } from 'react-feather';
+
 
 const Filters = () => {
   return (
-    <div class="space-y-2">
-          <details class="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden">
-            <summary class="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition">
-              <span class="text-sm font-medium"> Населено място </span>
-              <ChevronDown/>
-            </summary>
+    <div id="Filters">
+     <div className="flex flex-col justify-between bg-white rounded-xl mt-4">
+  <div className="">
+    <nav aria-label="Main Nav" className=" flex flex-col space-y-1">
 
-            <div class="border-t border-gray-200 bg-white ">
-              <header class="flex items-center justify-between p-4">
-                <span class="text-sm text-gray-700"> 0 Избрани </span>
+      <details className="group duration-100">
+        <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+          <div className="flex items-center gap-2">
+            <MapPin className='w-4 h-4'/>
+            <span className="text-md font-semibold"> Локация </span>
+          </div>
+          <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+          <ChevronDown/>
+          </span>
+        </summary>
 
-                <button type="button" class="text-sm text-gray-900 underline underline-offset-4">
-                  Нулиране
-                </button>
-              </header>
-
-              <ul class="space-y-1 border-t border-gray-200 p-4">
-                <li>
-                  <label for="FilterInStock" class="inline-flex items-center gap-2">
-                    <input type="checkbox" id="FilterInStock" class="h-5 w-5 rounded border-gray-300" />
-                    <span class="text-sm font-medium text-gray-700">
-                      test
-                    </span>
-                  </label>
-                </li>
-
-                <li>
-                  <label for="FilterPreOrder" class="inline-flex items-center gap-2">
-                    <input type="checkbox" id="FilterPreOrder" class="h-5 w-5 rounded border-gray-300"/>
-                    <span class="text-sm font-medium text-gray-700">
-                      test
-                    </span>
-                  </label>
-                </li>
-
-                <li>
-                  <label for="FilterOutOfStock" class="inline-flex items-center gap-2">
-                    <input type="checkbox" id="FilterOutOfStock" class="h-5 w-5 rounded border-gray-300" />
-                    <span class="text-sm font-medium text-gray-700">
-                      test
-                    </span>
-                  </label>
-                </li>
-              </ul>
-            </div>
-          </details>
-
-          <details class="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden">
-            <summary class="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition">
-              <span class="text-sm font-medium"> Бранш </span>
-              <ChevronDown/>
-            </summary>
-
-            <div class="border-t border-gray-200 bg-white">
-              <header class="flex items-center justify-between p-4">
-                <span class="text-sm text-gray-700"> 0 Избрани </span>
-
-                <button type="button" class="text-sm text-gray-900 underline underline-offset-4">
-                  Нулиране
-                </button>
-              </header>
-
-              <ul class="space-y-1 border-t border-gray-200 p-4">
-                <li>
-                  <label for="FilterInStock" class="inline-flex items-center gap-2">
-                    <input type="checkbox" id="FilterInStock" class="h-5 w-5 rounded border-gray-300" />
-                    <span class="text-sm font-medium text-gray-700">
-                      Хотел
-                    </span>
-                  </label>
-                </li>
-
-                <li>
-                  <label for="FilterPreOrder" class="inline-flex items-center gap-2">
-                    <input type="checkbox" id="FilterPreOrder" class="h-5 w-5 rounded border-gray-300"/>
-                    <span class="text-sm font-medium text-gray-700">
-                      Ресторант
-                    </span>
-                  </label>
-                </li>
-
-                <li>
-                  <label for="FilterOutOfStock" class="inline-flex items-center gap-2">
-                    <input type="checkbox" id="FilterOutOfStock" class="h-5 w-5 rounded border-gray-300" />
-                    <span class="text-sm font-medium text-gray-700">
-                      Магазин
-                    </span>
-                  </label>
-                </li>
-              </ul>
-            </div>
-          </details>
-
-          <details class="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden" >
-            <summary class="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition">
-              <span class="text-sm font-medium"> Заплата </span>
-              <ChevronDown/>
-            </summary>
-
-            <div class="border-t border-gray-200 bg-white">
-
-              <div class="border-t border-gray-200 p-4">
-                <div class="flex justify-between gap-4">
-                  <label for="FilterPriceFrom" class="flex items-center gap-2">
-                    <span class="text-sm text-gray-600">$</span>
-
-                    <input type="number" id="FilterPriceFrom" placeholder="От" class="w-full rounded-md border-gray-200 shadow-sm sm:text-sm" />
-                  </label>
-
-                  <label for="FilterPriceTo" class="flex items-center gap-2">
-                    <span class="text-sm text-gray-600">$</span>
-
-                    <input type="number" id="FilterPriceTo" placeholder="До" class="w-full rounded-md border-gray-200 shadow-sm sm:text-sm" />
-                  </label>
-                </div>
+        <nav aria-label="Location" className="mt-2 flex flex-col px-4 trasition duration-100">
+          <ul class="h-48 px-3 pb-3 overflow-y-auto text-sm text-gray-700" aria-labelledby="dropdownSearchButton">
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded " />
+                <label for="checkbox-item-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded">Бургас</label>
               </div>
-            </div>
-          </details>
-        </div>
+            </li>
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded " />
+                <label for="checkbox-item-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded">Поморие</label>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded " />
+                <label for="checkbox-item-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded">Сл. Бряг</label>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded " />
+                <label for="checkbox-item-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded">Лозенец</label>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded " />
+                <label for="checkbox-item-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded">Созопол</label>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded " />
+                <label for="checkbox-item-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded">Варна</label>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded " />
+                <label for="checkbox-item-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded">Несебър</label>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </details>
+
+      <details className="group duration-100">
+        <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+          <div className="flex items-center gap-2">
+            <Briefcase className='w-4 h-4'/>
+            <span className="text-md font-semibold">Бранш </span>
+          </div>
+          <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+          <ChevronDown/>
+          </span>
+        </summary>
+
+        <nav aria-label="Location" className="mt-2 flex flex-col px-4 trasition duration-100">
+          <ul class="h-48 px-3 pb-3 overflow-y-auto text-sm text-gray-700" aria-labelledby="dropdownSearchButton">
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded " />
+                <label for="checkbox-item-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded">Хотел</label>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded " />
+                <label for="checkbox-item-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded">Ресторант</label>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded " />
+                <label for="checkbox-item-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded">Магазин</label>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded " />
+                <label for="checkbox-item-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded">Спасител</label>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded " />
+                <label for="checkbox-item-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded">Аниматор</label>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded " />
+                <label for="checkbox-item-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded">Нещо</label>
+              </div>
+            </li>
+            <li>
+              <div class="flex items-center p-2 rounded hover:bg-gray-100">
+                <input id="checkbox-item-1" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded " />
+                <label for="checkbox-item-1" class="w-full ml-2 text-sm font-medium text-gray-900 rounded">Нещо #2</label>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </details>
+
+      <details className="group duration-100">
+        <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+          <div className="flex items-center gap-2">
+            <DollarSign className='w-6 h-6'/>
+            <span className="text-md font-semibold"> Само обяви с заплати </span>
+          </div>
+          <label for="AcceptConditions" class="relative h-6 w-14 cursor-pointer">
+            <input
+              type="checkbox"
+              id="AcceptConditions"
+              class="peer sr-only [&:checked_+_span_svg[data-checked-icon]]:block [&:checked_+_span_svg[data-unchecked-icon]]:hidden"
+            />
+
+            <span class="absolute inset-y-0 start-0 z-10 m-1 inline-flex h-4 w-4 items-center justify-center rounded-full bg-white text-gray-400 transition-all peer-checked:start-6 peer-checked:text-green-600">
+              <XCircle data-unchecked-icon/>
+              <CheckCircle data-checked-icon class="hidden h-4 w-4"/>
+            </span>
+            
+            <span class="absolute inset-0 rounded-full bg-gray-300 transition peer-checked:bg-green-500"></span>
+          </label>
+        </summary>
+      </details>
+
+    </nav>
+  </div>
+</div>
+    </div>
   )
 }
 
 export default Filters
+
