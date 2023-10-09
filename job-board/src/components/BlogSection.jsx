7dc6e2c9eb19968d89c1a3837364aed9f49aa5f9
alansshots <alengospodinov@gmail.com>
2023-10-09 19:29:47 +0200

    home page minor changes

--- job-board/src/components/BlogSection.jsx
+++ job-board/src/components/BlogSection.jsx
@@ -1,6 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { useQuery, gql } from "@apollo/client";
+
+const BLOGS_QUERY = gql`
+{
+posts {
+  date
+  id    
+  coverPhoto {
+    url
+    id
+  }
+  content {
+        html
+       }
+  title
+  tags
+  summary
+  slug
+  publishedBy {
+    name
+    picture
+    publishedAt
+  }
+}
+}
+`;
 
 const BlogSection = () => {
+
+  const { data, loading, error} = useQuery(BLOGS_QUERY);
+  if (loading) return (
+    <div className="absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2 ">
+      <div className="border-t-transparent border-solid animate-spin  rounded-full border-[#0146b1] border-8 h-64 w-64"></div>
+    </div>
+  );
   return (
     <div id='BlogSection'>
         <div className='bg-gray-100 pb-16'>
@@ -9,77 +43,31 @@ const BlogSection = () => {
             </div>
 
             <div className='m-auto max-w-6xl mt-10 flex flex-row'>
-                <article className="mx-5 animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-[length:400%_400%] p-0.5 shadow-md transition [animation-duration:_6s] hover:shadow-xl hover:scale-105">
+
+                {data.posts.map((post) => (
+                <Link to={"/blog/" + post.slug} className="mx-5 animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-[length:400%_400%] p-0.5 shadow-md transition [animation-duration:_6s] hover:shadow-xl hover:scale-105">
                     <div className="rounded-[10px] bg-white p-4 !pt-20 sm:p-6">
                         <time dateTime="2022-10-10" className="block text-xs text-gray-500">
-                        10/08/2023
+                        {post.date}
                         </time>
 
-                        <a href="#" className='hover:underline'>
+                        <a className='hover:underline'>
                         <h3 className="mt-0.5 text-lg font-medium text-gray-900">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, doloremque.
+                            {post.summary}
                         </h3>
                         </a>
 
                         <div className="mt-4 flex flex-wrap gap-1">
-                        <span className="whitespace-nowrap rounded-full bg-[#0852bf] px-2.5 py-0.5 text-xs text-[#b3fd49]" >
-                            хаштаг
-                        </span>
-
-                        <span className="whitespace-nowrap rounded-full bg-[#0852bf] px-2.5 py-0.5 text-xs text-[#b3fd49]" >
-                            хаштаг
-                        </span>
+                            {post.tags.map((post) => (
+                                <span className="whitespace-nowrap rounded-full bg-[#0852bf] px-2.5 py-0.5 text-xs text-white" >
+                                    {post}
+                                </span>
+                            ))}
                         </div>
                     </div>
-                </article>
-
-                <article className="mx-5 animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-[length:400%_400%] p-0.5 shadow-md transition [animation-duration:_6s] hover:shadow-xl hover:scale-105">
-                    <div className="rounded-[10px] bg-white p-4 !pt-20 sm:p-6">
-                        <time dateTime="2022-10-10" className="block text-xs text-gray-500">
-                        15/03/2023
-                        </time>
-
-                        <a href="#" className='hover:underline'>
-                        <h3 className="mt-0.5 text-lg font-medium text-gray-900">
-                            Lorem ipsum dolor sit amet consectetur adipisicing.
-                        </h3>
-                        </a>
-
-                        <div className="mt-4 flex flex-wrap gap-1">
-                        <span className="whitespace-nowrap rounded-full bg-[#0852bf] px-2.5 py-0.5 text-xs text-[#b3fd49]" >
-                            хаштаг
-                        </span>
-
-                        <span className="whitespace-nowrap rounded-full bg-[#0852bf] px-2.5 py-0.5 text-xs text-[#b3fd49]" >
-                            хаштаг
-                        </span>
-                        </div>
-                    </div>
-                </article>
-
-                <article className="mx-5 animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-[length:400%_400%] p-0.5 shadow-md transition [animation-duration:_6s] hover:shadow-xl hover:scale-105">
-                    <div className="rounded-[10px] bg-white p-4 !pt-20 sm:p-6">
-                        <time dateTime="2022-10-10" className="block text-xs text-gray-500">
-                        07/12/2023
-                        </time>
-
-                        <a href="#" className='hover:underline'>
-                        <h3 className="mt-0.5 text-lg font-medium text-gray-900">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing.
-                        </h3>
-                        </a>
-
-                        <div className="mt-4 flex flex-wrap gap-1">
-                        <span className="whitespace-nowrap rounded-full bg-[#0852bf] px-2.5 py-0.5 text-xs text-[#b3fd49]" >
-                            хаштаг
-                        </span>
-
-                        <span className="whitespace-nowrap rounded-full bg-[#0852bf] px-2.5 py-0.5 text-xs text-[#b3fd49]" >
-                            хаштаг
-                        </span>
-                        </div>
-                    </div>
-                </article>
+                </Link>
+                ))}
 
             </div>
         </div>
--- job-board/src/components/LatestJobs.jsx
+++ job-board/src/components/LatestJobs.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
+import supabase from '../config/supabaseClient'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 const LatestJobs = () => {
+
+    const [fetchError, setFetchError] = useState(null)
+    const [offers, setOffers] = useState(null)
+  
+  useEffect(() => {
+    const fetchOffers = async () => {
+      const { data, error } = await supabase
+        .from('offers')
+        .select('*')
+        .order('created_at', { ascending: false }) 
+        .limit(3);
+        
+        if (error) {
+          setFetchError('Could not fetch the offers')
+          setOffers(null)
+        }
+        if (data) {
+          setOffers(data);
+          setFetchError(null)
+        }
+      }
+  
+      fetchOffers()
+    }, [])
+
   return (
     <div className='bg-gray-100 pb-16'>
         <div className='m-auto max-w-6xl pt-12 bg-gray-100'>
@@ -8,122 +36,46 @@ const LatestJobs = () => {
         </div>
         
         <div className='m-auto max-w-6xl mt-16 flex flex-row items-center'>
-            <div className='flex flex-col'>
-                <div className='flex flex-row items-center border-l-4 border-gray-300 p-2 hover:opacity-80 trasition duration-100 cursor-pointer'>
-                    <h3>Product Managment</h3> 
-                    <span className='ml-2 px-2 py-1 bg-blue-100 rounded-xl'>34</span>
-                </div>
+        {fetchError && (<p>{fetchError}</p>)}
+            {offers && (
+            <div className='flex flex-row items-center justify-between w-full'>
+                {offers.map(offer => (
+                <div key={offer.id} className='bg-white w-5/12 mx-6 flex-col justify-center items-center p-6 rounded-2xl cursor-pointer transition duration-200 hover:scale-105 hover:shadow-xl shadow-md'>
+                    <h2 className='text-2xl text-black font-semibold'>{offer.title}</h2>
+                    
+                    <div className='mt-2'>
+                        <span className='bg-[#16368d] text-sm rounded-xl text-white px-2 py-1 mr-2'>{offer.location}</span>
+                        {offer.salary && (
+                          <span className='bg-[#16368d] text-sm rounded-xl text-white px-2 py-1 mr-2'>{offer.salary} лв</span>
+                        )
+                        }
+                    
+                    </div>
+                    
+                    <h2 className='mt-5 text-xl font-semibold text-[#0146b1]'>{offer.experience}</h2>
+                    
+                    <div className='flex flex-col justify-center items-left mt-16 border-t boreder-gray-100 pt-2'>
+                        
+                        <div className=" mb-2 flex flex-row justify-left items-center">
+                          <a href="">
+                                <img className='rounded-full w-10 h-10' src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
+                          </a>
+                          <div>
+                            <a href="" className='text-[#0146b1] text-sm ml-0.5'>{offer.author}</a>
+                          </div>
+                        </div>
 
-                <div className='flex flex-row items-center border-l-4 border-[#0146b1] p-2 hover:opacity-80 trasition duration-100 cursor-pointer'>
-                    <h3>Design</h3> 
-                    <span className='ml-2 px-2 py-1 bg-gray-200 rounded-xl'>34</span>
-                </div>
+                        <div>
+                            <Link to={"offers/" + offer.slug} className='bg-[#0146b1] w-20 text-white transition duration-200 hover:scale-105 text-sm px-1 py-0.5 rounded-xl flex flex-row item-center justify-between'>
+                                Свържи се
+                            </Link>
+                        </div>
 
-                <div className='flex flex-row items-center border-l-4 border-gray-300 p-2 hover:opacity-80 trasition duration-100 cursor-pointer'>
-                    <h3>Marketing</h3> 
-                    <span className='ml-2 px-2 py-1 bg-red-100 rounded-xl'>34</span>
+                    </div>
                 </div>
-
-                <div className='flex flex-row items-center border-l-4 border-gray-300 p-2 hover:opacity-80 trasition duration-100 cursor-pointer'>
-                    <h3>Customer Service</h3> 
-                    <span className='ml-2 px-2 py-1 bg-green-100 rounded-xl'>34</span>
-                </div>
+                ))}
             </div>
-
-            <div className='ml-10 flex flex-row items-center justify-between'>
-
-                {/* <div className='bg-[#0852bf] mx-10 flex-col justify-center items-center p-6 rounded-2xl cursor-pointer transition duration-200 hover:scale-105 hover:shadow-xl shadow-md'>
-                    <h2 className='text-2xl text-white font-semibold'>Product Designer</h2>
-                    
-                    <div className='mt-2'>
-                        <span className='bg-[#16368d] text-sm rounded-xl text-white px-2 py-1 mr-2'>Full Time</span>
-                        <span className='bg-[#16368d] text-sm rounded-xl text-white px-2 py-1 mr-2'>Product</span>
-                    </div>
-                    
-                    <h2 className='mt-5 text-xl font-semibold text-[#b3fd49]'>1000-1500 USD</h2>
-                    
-                    <div className='flex flex-row justify-between items-center mt-16 border-t boreder-gray-100 pt-2'>
-                        
-                        <div className="flex flex-row justify-center items-center">
-                          <a href="">
-                                <img className='rounded-full w-10 h-10' src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
-                          </a>
-                          <div>
-                            <a href="" className='text-white text-sm ml-0.5'>Colorblock</a>
-                          </div>
-                        </div>
-
-                        <div>
-                            <a href='' className='bg-[#b3fd49] transition duration-200 hover:scale-105 text-sm px-1 py-0.5 rounded-xl flex flex-row item-center justify-between'>
-                                Свържи се
-                            </a>
-                        </div>
-
-                    </div>
-                </div> */}
-
-                <div className='bg-white mx-6 flex-col justify-center items-center p-6 rounded-2xl cursor-pointer transition duration-200 hover:scale-105 hover:shadow-xl shadow-md'>
-                    <h2 className='text-2xl text-black font-semibold'>Product Designer</h2>
-                    
-                    <div className='mt-2'>
-                        <span className='bg-[#16368d] text-sm rounded-xl text-white px-2 py-1 mr-2'>Full Time</span>
-                        <span className='bg-[#16368d] text-sm rounded-xl text-white px-2 py-1 mr-2'>Product</span>
-                    </div>
-                    
-                    <h2 className='mt-5 text-xl font-semibold text-[#0146b1]'>1000-1500 USD</h2>
-                    
-                    <div className='flex flex-row justify-between items-center mt-16 border-t boreder-gray-100 pt-2'>
-                        
-                        <div className="flex flex-row justify-center items-center">
-                          <a href="">
-                                <img className='rounded-full w-10 h-10' src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
-                          </a>
-                          <div>
-                            <a href="" className='text-[#0146b1] text-sm ml-0.5'>Colorblock</a>
-                          </div>
-                        </div>
-
-                        <div>
-                            <a href='' className='bg-[#0146b1] text-white transition duration-200 hover:scale-105 text-sm px-1 py-0.5 rounded-xl flex flex-row item-center justify-between'>
-                                Свържи се
-                            </a>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div className='bg-white mx-6 flex-col justify-center items-center p-6 rounded-2xl cursor-pointer transition duration-200 hover:scale-105 hover:shadow-xl shadow-md'>
-                    <h2 className='text-2xl text-black font-semibold'>Product Designer</h2>
-                    
-                    <div className='mt-2'>
-                        <span className='bg-[#16368d] text-sm rounded-xl text-white px-2 py-1 mr-2'>Full Time</span>
-                        <span className='bg-[#16368d] text-sm rounded-xl text-white px-2 py-1 mr-2'>Product</span>
-                    </div>
-                    
-                    <h2 className='mt-5 text-xl font-semibold text-[#0146b1]'>1000-1500 USD</h2>
-                    
-                    <div className='flex flex-row justify-between items-center mt-16 border-t boreder-gray-100 pt-2'>
-                        
-                        <div className="flex flex-row justify-center items-center">
-                          <a href="">
-                                <img className='rounded-full w-10 h-10' src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
-                          </a>
-                          <div>
-                            <a href="" className='text-[#0146b1] text-sm ml-0.5'>Colorblock</a>
-                          </div>
-                        </div>
-
-                        <div>
-                            <a href='' className='bg-[#0146b1] text-white transition duration-200 hover:scale-105 text-sm px-1 py-0.5 rounded-xl flex flex-row item-center justify-between'>
-                                Свържи се
-                            </a>
-                        </div>
-
-                    </div>
-                </div>
-
-            </div>
-
+            )}
         </div>
 
     </div>
